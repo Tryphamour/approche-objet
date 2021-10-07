d09394e9236b7d98a0f7c281f6d7f88d4ae7b522
Felix <felix.gaillard51@gmail.com>
2021-10-07 16:44:15 +0200

    Ajout de méthodes

--- approche-objet/src/entites/TestPersonne.java
+++ approche-objet/src/entites/TestPersonne.java
@@ -10,7 +10,10 @@ public class TestPersonne {
 		AdressePostale adr2 = new AdressePostale(5, "Rue Victor Duruy", 51100, "Reims");
 
 		Personne prsn1 = new Personne("Gaillard", "Félix", adr1);
+		prsn1.setNom("Guenat");
+		prsn1.setPrenom("Julien");
 
 		Personne prsn2 = new Personne("John", "Doe", adr2);
+		prsn2.setAdresse(new AdressePostale (54, "Rue Clamart", 51000, "Châlons en Champagne"));
 	}
 }
--- approche-objet/src/entites2/Personne.java
+++ approche-objet/src/entites2/Personne.java
@@ -3,6 +3,7 @@ package entites2;
 import entites.AdressePostale;
 
 public class Personne {
+
 	public String nom;
 	public String prenom;
 	public AdressePostale adresse;
@@ -11,5 +12,33 @@ public class Personne {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.adresse = adresse;
+	}
+
+	public void main(String[] args) {
+		System.out.println(prenom + " " + nom.toUpperCase());
+	}
+
+	public void setNom(String nvNom) {
+		this.nom = nvNom;
+	}
+
+	public void setPrenom(String nvPrenom) {
+		this.prenom = nvPrenom;
+	}
+	
+	public void setAdresse(AdressePostale nvAdresse) {
+		this.adresse = nvAdresse;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
+	}
+	
+	public AdressePostale getAdresse() {
+		return this.adresse;
 	}
 }
