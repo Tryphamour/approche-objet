5b48e5e722dd89128dd156d57b79d37b0de2f956
Felix <felix.gaillard51@gmail.com>
2021-10-07 15:05:32 +0200

    ajout de constucteurs

--- approche-objet/src/entites/TestAdressePostale.java
+++ approche-objet/src/entites/TestAdressePostale.java
@@ -3,16 +3,8 @@ package entites;
 public class TestAdressePostale {
 
 	public static void main(String[] args) {
-		AdressePostale adr1 = new AdressePostale();
-		adr1.numeroRue = 1;
-		adr1.libelleRue = "Rue Léon Bourgeois";
-		adr1.codePostal = 51000;
-		adr1.ville = "Châlons en Champagne";
+		AdressePostale adr1 = new AdressePostale(1, "Rue Léon Bourgeois", 51000, "Châlons en Champagne");
 
-		AdressePostale adr2 = new AdressePostale();
-		adr2.numeroRue = 5;
-		adr2.libelleRue = "Rue Victor Duruy";
-		adr2.codePostal = 51100;
-		adr2.ville = "Reims";
+		AdressePostale adr2 = new AdressePostale(5, "Rue Victor Duruy", 51100, "Reims");
 	}
 }
--- approche-objet/src/entites/TestPersonne.java
+++ approche-objet/src/entites/TestPersonne.java
@@ -6,27 +6,11 @@ public class TestPersonne {
 
 	public static void main(String[] args) {
 		
-		AdressePostale adr1 = new AdressePostale();
-		adr1.numeroRue = 1;
-		adr1.libelleRue = "Rue Léon Bourgeois";
-		adr1.codePostal = 51000;
-		adr1.ville = "Châlons en Champagne";
-		
-		AdressePostale adr2 = new AdressePostale();
-		adr2.numeroRue = 5;
-		adr2.libelleRue = "Rue Victor Duruy";
-		adr2.codePostal = 51100;
-		adr2.ville = "Reims";
-		
-		Personne prsn1 = new Personne();
-		prsn1.nom = "Gaillard";
-		prsn1.prenom = "Félix";
-		prsn1.adresse = new AdressePostale();
-		prsn1.adresse = adr1;
+		AdressePostale adr1 = new AdressePostale(1, "Rue Léon Bourgeois", 51000, "Châlons en Champagne");
+		AdressePostale adr2 = new AdressePostale(5, "Rue Victor Duruy", 51100, "Reims");
 
-		Personne prsn2 = new Personne();
-		prsn2.nom = "John";
-		prsn2.prenom = "Doe";
-		prsn2.adresse = adr2;
+		Personne prsn1 = new Personne("Gaillard", "Félix", adr1);
+
+		Personne prsn2 = new Personne("John", "Doe", adr2);
 	}
 }
