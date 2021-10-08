1259a4b12d1eedf69ebd63853ef71df5f3135f87
Felix <felix.gaillard51@gmail.com>
2021-10-08 12:21:37 +0200

    encapsulation

---
+++ approche-objet/src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,38 @@
+package fr.diginamic.banque.entites;
+
+/**
+ * @author Félix Gaillard
+ *
+ */
+
+public class Compte {
+	private int numeroCompte;
+	private double soldeCompte;
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param numeroCompte numéro de compte
+	 * @param soldeCompte  solde du compte
+	 */
+	public Compte(int numeroCompte, double soldeCompte) {
+		this.numeroCompte = numeroCompte;
+		this.soldeCompte = soldeCompte;
+	}
+
+	public int getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public void setNumeroCompte(int numeroCompte) {
+		this.numeroCompte = numeroCompte;
+	}
+
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+
+	public void setSoldeCompte(double soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+}
---
+++ approche-objet/src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,10 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+
+		Compte cmpt = new Compte(123456, 9876.54);
+		System.out.println(cmpt);
+	}
+}
