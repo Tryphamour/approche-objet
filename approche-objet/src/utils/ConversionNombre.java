720085d7b35ac388ba9dddc77b791fc5aff58880
Felix <felix.gaillard51@gmail.com>
2021-10-08 10:26:14 +0200

    Ajout méthode static

---
+++ approche-objet/src/utils/ConversionNombre.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.lang.Integer;
+
+public class ConversionNombre {
+
+	String chaine = "12";
+
+	int integer = Integer.parseInt(chaine);
+
+	public static void main(String[] args) {
+
+		int a = 52;
+		int b = 7;
+
+		System.out.println(Math.max(a, b));
+	}
+}
